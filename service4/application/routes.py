a984835b6ab5bb937aafa184c33edfe3fc39b67e
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-13 08:48:28 +0000

    services

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -1,8 +1,52 @@
-from flask import Flask, render_template
-import random 
+from flask import Flask, Response, request
+import random
+from application import app
 
 
 @app.route('/price', methods=['GET', 'POST'])
+
+
+if city == "London":
+        if activity == 'Paintballing':
+            price = '£200 -£400'
+        elif activity == 'Surfing':
+            price = '£400 - £800'
+        elif activity == 'Scuba Diving':
+            price = '£800 - £1000'
+        elif activity == 'Scuba Diving':
+            price = '£1000 - £2000'
+     elif city == "Barcelona":
+        if activity == 'Paintballing':
+            price = '£200 -£400'
+        elif activity == 'Surfing':
+            price = '£400 - £800'
+        elif activity == 'Scuba Diving':
+            price = '£800 - £1000'
+        elif activity == 'Scuba Diving':
+            price = '£1000 - £2000'
+    elif city == "Milan":
+        if activity == 'Paintballing':
+            price = '£200 -£400'
+        elif activity == 'Surfing':
+            price = '£400 - £800'
+        elif activity == 'Scuba Diving':
+            price = '£800 - £1000'
+        elif activity == 'Scuba Diving':
+            price = '£1000 - £2000'
+    elif city == "Tokyo":
+        if activity == 'Paintballing':
+            price = '£200 -£400'
+        elif activity == 'Surfing':
+            price = '£400 - £800'
+        elif activity == 'Scuba Diving':
+            price = '£800 - £1000'
+        elif activity == 'Scuba Diving':
+            price = '£1000 - £2000'
+            
+    else:
+        return "Random Holiday could not be genrated" 
+
+return Response(price)
 
 #Get country from service 2 
 
