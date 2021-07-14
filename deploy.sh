c3e879c66614334c185a54f710bedb5a3a425589
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-14 10:07:59 +0000

    compose file mostly completed

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ docker build -t service_4_api service4
 
 # Run containers 
 
-docker run -d --name service_1 --network service_1_network service_1_server
+docker run -d --name -p 5000:5000 service_1 --network service_1_network service_1_server
 
 docker run -d --name service_2_api --network service_1_network service_2_api
 
@@ -25,4 +25,3 @@ docker run -d --name service_3_api --network service_1_network service_3_api
 docker run -d --name service_4_api --network service_1_network service_4_api
 
 
-#xpose on port 5000
