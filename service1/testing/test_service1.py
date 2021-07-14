e5667df2ed8a5719bb1371618729f38d079c51e7
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-14 15:10:06 +0000

    tests configured except for service 4

--- service1/testing/test_service1.py
+++ service1/testing/test_service1.py
@@ -11,23 +11,23 @@ class TestBase(TestCase):
         return app
 
 
-# class TestResponse(TestBase):
-#     def test_service1(self):
-#         with patch("requests.get") as a:
-#             with patch("requests.get") as c: 
-#                 with patch("requests.post") as b:
-#                     a.return_city.text = "London"
-#                     c.return_activity = "Paintballing"
-#                     b.return_price.text = "£200 - £400"
-#                 response = self.client.get(url_for('/'))
-#                 self.assertIn(b'The total cost of your holiday will be between £200 - £400', response.data)
-
-
-class TestService1(TestBase):
+class TestResponse(TestBase):
     def test_service1(self):
-        with requests_mock.Mocker() as mocker:
-            mocker.get("http://service_2_api:5000/city", text='London')
-            mocker.post("http://service_4_api:5000/price", text='£200 - £400') 
-        response = self.client.get(url_for('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("The total cost of your holiday will be between £200 - £400", response.data)
+        with patch("requests.get") as a:
+            # with patch("requests.get") as c: 
+                with patch("requests.post") as b:
+                    a.return_city.text = "London"
+                    # c.return_activity = "Paintballing"
+                    b.return_price.text = "£200 - £400"
+                response = self.client.get(url_for('index'))
+                self.assertIn(b'The total cost of your holiday will be between £200 - £400', response.data)
+
+
+# class TestService1(TestBase):
+#     def test_service1(self):
+#         with requests_mock.Mocker() as mocker:
+#             mocker.get("http://service_2_api:5000/city", text='London')
+#             mocker.post("http://service_4_api:5000/price", text='£200 - £400') 
+#         response = self.client.get(url_for('index'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn("The total cost of your holiday will be between £200 - £400", response.data)
