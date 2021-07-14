8523c313aa58e3dcad6d51814a710b59fbdd7543
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-14 08:39:44 +0000

    docker containers all working

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,11 @@ docker build -t service_4_api service4
 
 # create networks 
 
-docker network create service_1_network 
+# docker network create service_1_network 
 
 # Run containers 
+
+docker run -d --name service_1 --network service_1_network service_1_server
 
 docker run -d --name service_2_api --network service_1_network service_2_api
 
@@ -23,4 +25,4 @@ docker run -d --name service_3_api --network service_1_network service_3_api
 docker run -d --name service_4_api --network service_1_network service_4_api
 
 
-
+#xpose on port 5000
--- service4/application/routes.py
+++ service4/application/routes.py
@@ -5,52 +5,54 @@ from application import app
 
 @app.route('/price', methods=['GET', 'POST'])
 
-data_sent = request.data.decode('utf-8')
-data = data_sent.split(" ")
-activity = data[0]
-city = data[1]
+def price(): 
 
-if city == "London":
-    if activity == 'Paintballing':
-            price = '£200 -£400'
-    elif activity == 'Surfing':
-            price = '£400 - £800'
-    elif activity == 'Scuba Diving':
-            price = '£800 - £1000'
-    elif activity == 'Scuba Diving':
-            price = '£1000 - £2000'
-elif city == "Barcelona":
-    if activity == 'Paintballing':
-            price = '£200 -£400'
-    elif activity == 'Surfing':
-            price = '£400 - £800'
-    elif activity == 'Scuba Diving':
-            price = '£800 - £1000'
-    elif activity == 'Scuba Diving':
-            price = '£1000 - £2000'
-elif city == "Milan":
-    if activity == 'Paintballing':
-            price = '£200 -£400'
-    elif activity == 'Surfing':
-            price = '£400 - £800'
-    elif activity == 'Scuba Diving':
-            price = '£800 - £1000'
-    elif activity == 'Scuba Diving':
-            price = '£1000 - £2000'
-elif city == "Tokyo":
-    if activity == 'Paintballing':
-            price = '£200 -£400'
-    elif activity == 'Surfing':
-            price = '£400 - £800'
-    elif activity == 'Scuba Diving':
-            price = '£800 - £1000'
-    elif activity == 'Scuba Diving':
-            price = '£1000 - £2000'
-            
-else:
-    return "Random Holiday could not be generated" 
+        data_sent = request.data.decode('utf-8')
+        data = data_sent.split(" ")
+        activity = data[0]
+        city = data[1]
 
-return Response(price, mimetype="text/plain")
+        if city == "London":
+                if activity == 'Paintballing':
+                        price = '£200 - £400'
+                elif activity == 'Surfing':
+                        price = '£400 - £800'
+                elif activity == 'Scuba Diving':
+                        price = '£800 - £1000'
+                elif activity == 'Scuba Diving':
+                        price = '£1000 - £2000'
+        elif city == "Barcelona":
+                if activity == 'Paintballing':
+                        price = '£200 - £400'
+                elif activity == 'Surfing':
+                        price = '£400 - £800'
+                elif activity == 'Scuba Diving':
+                        price = '£800 - £1000'
+                elif activity == 'Scuba Diving':
+                        price = '£1000 - £2000'
+        elif city == "Milan":
+                if activity == 'Paintballing':
+                        price = '£200 - £400'
+                elif activity == 'Surfing':
+                        price = '£400 - £800'
+                elif activity == 'Scuba Diving':
+                        price = '£800 - £1000'
+                elif activity == 'Scuba Diving':
+                        price = '£1000 - £2000'
+        elif city == "Tokyo":
+                if activity == 'Paintballing':
+                        price = '£200 - £400'
+                elif activity == 'Surfing':
+                        price = '£400 - £800'
+                elif activity == 'Scuba Diving':
+                        price = '£800 - £1000'
+                elif activity == 'Scuba Diving':
+                        price = '£1000 - £2000'
+                
+        else:
+                return "Random Holiday could not be generated" 
+
+        return Response(price, mimetype="text/plain")
 
 
 
