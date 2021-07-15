46ce6874720749437fb13866e3288ae83028f646
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-15 10:46:12 +0000

    service1 test and coverage working

--- service1/testing/test_service1.py
+++ service1/testing/test_service1.py
@@ -1,25 +1,12 @@
 from flask import url_for
 from flask_testing import TestCase
-from unittest.mock import patch
 import requests_mock
 from app import app
 
 class TestBase(TestCase):
     def create_app(self):
-        app.config.update(DEBUG=True)
+        # app.config.update(DEBUG=True)
         return app
-
-# class TestResponse(TestBase):
-#     def test_service1(self):
-#         with patch("requests.get") as a:
-#                 with patch("requests.get") as b:
-#                     with patch("requests.post") as c:
-#                         a.return_value.text = "London"
-#                         b.return_value.text = "Paintballing"
-#                         c.return_value.text = "200"
-
-#                 response = self.client.get(url_for('index'))
-#                 self.assertIn(b'The total cost of your holiday will be 200 GBP', response.data)
 
 class TestService1(TestBase):
     def test_service1(self):
@@ -27,6 +14,15 @@ class TestService1(TestBase):
             mocker.get("http://service_2_api:5000/city", text='London')
             mocker.get("http://service_3_api:5000/activity", text='Paintballing')
             mocker.post("http://service_4_api:5000/price", text='200') 
-        response = self.client.get(url_for('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'The total cost of your holiday will 200 GBP', response.data)
+            response = self.client.get(url_for('index'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'The total cost of your holiday will be 200 GBP', response.data)
+
+    # def test_service1(self):
+    #     with requests_mock.Mocker() as mocker:
+    #         mocker.get("http://service_2_api:5000/city", text='Milan')
+    #         mocker.get("http://service_3_api:5000/activity", text='Surfing')
+    #         mocker.post("http://service_4_api:5000/price", text='400') 
+    #         response = self.client.get(url_for('index'))
+    #         self.assertEqual(response.status_code, 200)
+    #         self.assertIn(b'The total cost of your holiday will be 400 GBP', response.data)
