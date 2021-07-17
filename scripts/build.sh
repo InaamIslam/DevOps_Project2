d320691a80c6acbfda3d2665dca237353a56e039
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-17 14:55:45 +0000

    jenkinsfile, docker-compose, scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 docker --version
+curl https://get.docker.com | sudo bash
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 docker-compose down --rmi all
 docker-compose build
 docker-compose push 
