d89f84dd750fd98de8d66621a651eab7b8b1862e
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-18 16:06:58 +0000

    configure jenkinsfile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 ssh -i ~/.ssh/ansible_id_rsa jenkins@ansible << EOF
-
 sudo docker node ls
-git clone 
-cd 
 docker login
 sudo docker pull inaamiii/random_holiday_service1
 sudo docker pull inaamiii/service_2_api
 sudo docker pull inaamiii/service_3_api
 sudo docker pull inaamiii/service_4_api
-
-
 sudo docker stack deploy -c docker-compose.yaml 
 EOF
 
