d6b1f85f3b50afc8f980a2c25aad3550e852d4e8
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-18 10:10:39 +0000

    updates to jenkins stages script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,14 @@
-#!/bin/bash
+#! /bin/bash
+ssh -i ~/.ssh/ansible_id_rsa jenkins@ansible << EOF
+docker-compose up -d
 
-# build server
+# sudo docker node ls
+# git clone 
+# cd W9_-SoloProject
+# docker login
+# sudo docker pull (pull service images)
+# sudo docker stack deploy -c docker-compose.yaml 
+# EOF
 
-docker build -t service_1_server service1
-
-# build api's
-
-docker build -t service_2_api service2
-docker build -t service_3_api service3
-docker build -t service_4_api service4
-
-# create networks 
-
-# docker network create service_1_network 
-
-# Run containers 
-
-docker run -d -p 5000:5000 --name service_1 --network service_1_network service_1_server
-
-docker run -d --name service_2_api --network service_1_network service_2_api
-
-docker run -d --name service_3_api --network service_1_network service_3_api
-
-docker run -d --name service_4_api --network service_1_network service_4_api
 
 
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,27 +2,27 @@
 echo "----Testing service 1-----"
 cd service1/
 pip3 install -r requirements.txt
-python3 -m pytest --cov . --cov-report term-missing
+python3 -m pytest --cov . --cov-report html
 
 cd ..
 
 echo "----Testing service 2-----"
 cd service2/
 pip3 install -r requirements.txt
-python3 -m pytest --cov . --cov-report term-missing
+python3 -m pytest --cov . --cov-report html
 
 cd ..
 
 echo "----Testing service 3-----"
 cd service3/
 pip3 install -r requirements.txt
-python3 -m pytest --cov . --cov-report term-missing
+python3 -m pytest --cov . --cov-report html
 
 cd ..
 
 echo "----Testing service 4-----"
 cd service4/
 pip3 install -r requirements.txt
-python3 -m pytest --cov . --cov-report term-missing
+python3 -m pytest --cov . --cov-report html
 
 echo "----Testing completed----"
