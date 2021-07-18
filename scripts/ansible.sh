e8f449c347a5ccdff436ebc892d9a4adcc0aace8
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-18 16:39:49 +0000

    jenkinsfile updates

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#! /bin/bash
 ~/.local/bin/ansible-playbook -i inventory playbook.yaml
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,22 +1,15 @@
-# #!/bin/bash
+#! /bin/bash
 sudo apt update 
 sudo apt install -y curl jq
-
-#install docker
-
 curl https://get.docker.com | sudo bash 
 sudo usermod -a -G docker jenkins 
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-#install ansible
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python3-pip3 -y
 pip3 install --user ansible
 ansible --version
-
-#docker login 
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
