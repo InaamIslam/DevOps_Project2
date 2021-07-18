c9ac35bc3d00bdb5d8b2340409f56589d728d41e
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-18 14:53:45 +0000

    jenkinsfile update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 ssh -i ~/.ssh/ansible_id_rsa jenkins@ansible << EOF
-docker-compose up -d
 
-# sudo docker node ls
-# git clone 
-# cd W9_-SoloProject
-# docker login
-# sudo docker pull (pull service images)
-# sudo docker stack deploy -c docker-compose.yaml 
-# EOF
+sudo docker node ls
+git clone 
+cd 
+docker login
+sudo docker pull inaamiii/random_holiday_service1
+sudo docker pull inaamiii/service_2_api
+sudo docker pull inaamiii/service_3_api
+sudo docker pull inaamiii/service_4_api
+
+
+sudo docker stack deploy -c docker-compose.yaml 
+EOF
 
 
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-sudo apt install python3 python3-pip
-python3 -m pip install -U pip
-python3 -m pip install -r requirements.txt
+# #!/bin/bash
+# sudo apt install python3 python3-pip
+# python3 -m pip install -U pip
+# python3 -m pip install -r requirements.txt
