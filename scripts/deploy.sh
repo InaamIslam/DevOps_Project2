83b0570bc366c69acb3e288f056b8c6a7e17c73a
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-19 09:11:59 +0000

    updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-scp -o StrictHostKeyChecking=no -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@jenkins:/home/jenkins/docker-compose.yaml
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/ansible_id_rsa jenkins@jenkins
+scp -o StrictHostKeyChecking=no -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/ansible_id_rsa jenkins@manager
 << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
