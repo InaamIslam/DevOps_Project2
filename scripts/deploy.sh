6d22a920c10ba42ef2a837525c4fa32e3b21d597
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-19 08:40:05 +0000

    updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,17 +7,6 @@ ssh -i ~/.ssh/ansible_id_rsa jenkins@jenkins
     export SECRET_KEY=${SECRET_KEY}
     docker stack deploy --compose-file docker-compose.yaml encounters
 EOF
-ssh -i ~/.ssh/ansible_id_rsa jenkins@jenkins << EOF
-sudo docker node ls
-git clone https://github.com/InaamIslam/DevOps_Project2.git
-cd DevOps_Project2
-docker login 
-sudo docker pull inaamiii/random_holiday_service1
-sudo docker pull inaamiii/service_2_api
-sudo docker pull inaamiii/service_3_api
-sudo docker pull inaamiii/service_4_api
-sudo docker stack deploy -c docker-compose.yaml 
-EOF
 
 
 
