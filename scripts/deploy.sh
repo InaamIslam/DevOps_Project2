64f2b196ea0296cb1e29b4a9df6f900ce5bb90c1
Inaam Islam <Inaam.islaam@gmail.com>
2021-07-19 10:14:19 +0000

    updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 scp -o StrictHostKeyChecking=no -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/ansible_id_rsa jenkins@manager
-<< EOF
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
     docker stack deploy --compose-file docker-compose.yaml encounters
